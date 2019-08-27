aa9080099f8766686bbc90507d64ee15ea56a186
xiaozhihua <xiaozh@weipaitang.com>
2019-08-27 16:21:24 +0800

    feat: 添加nedb通用接口

---
+++ db.js
@@ -0,0 +1,50 @@
+/**
+ * @file dbTool
+ * @author xzh
+ * @date 2019-08-27 16:09:21
+ */
+
+const Nedb = require('nedb')
+
+module.exports = {
+    getDb: (function() {
+        const map = new Map()
+    
+        return (name) => {
+            let db = map.get(name)
+    
+            if (db) {
+                return db
+            }
+    
+            db = new Nedb({
+                filename: `./db/${name}`,
+                autoload: true,
+            })
+    
+            map.set(name, db)
+    
+            return db
+        }
+    }()),
+    insert: (db, docs) => {
+        return new Promise(resolve => {
+            db.insert(docs, (err, newDoc) => {
+                if (err) {
+                    resolve({
+                        code: 0,
+                        msg: '添加失败'
+                    })
+    
+                    return
+                }
+    
+                resolve({
+                    code: 1,
+                    data: newDoc,
+                    msg: 'ok'
+                })
+            })
+        })
+    }
+}
---
+++ index.js
@@ -0,0 +1,74 @@
+/**
+ * @file index
+ * @author xzh
+ * @date 2019-08-27 16:10:00
+ */
+
+const http = require('http')
+const dbTool = require('./db')
+
+const getJson = (r) => {
+    return new Promise(resolve => {
+        let str = ''
+    
+        r.on('data', data => str += data)
+        r.on('end', () => {
+            if (str) {
+                resolve(JSON.parse(str))
+                return
+            }
+            
+            resolve(null)
+        })
+    })
+}
+
+const send = (r, data) => {
+    r.writeHead(200, {
+        'Content-Type': 'application/json'
+    })
+
+    r.end(JSON.stringify(data))
+}
+
+http.createServer((req, res) => {
+    if (req.method !== 'POST') {
+        res.end('hello world')
+        return
+    }
+
+    const path = req.url.split('/api/')[1]
+    const [ name, type ] = path.split('/')
+    const fn = dbTool[type]
+
+    if (!name || type === 'getDb') {
+        send(res, {
+            code: 0,
+            msg: '请求路径错误'
+        })
+
+        return
+    }
+
+    if (!fn) {
+        send(res, {
+            code: 0,
+            msg: `找不到${type}这个方法，确定这是nedb方法？`
+        })
+    }
+
+    getJson(req).then(data => {
+        if (!data) {
+            send(res, {
+                code: 0,
+                msg: '参数错误'
+            })
+
+            return
+        }
+
+        fn(dbTool.getDb(name), data).then(info => {
+            send(res, info)
+        })
+    })
+}).listen(8014)
